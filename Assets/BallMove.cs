bbea3853088a5299ea7d73740ebe99b77bd53c9f
ryanebani <ryanebani@gmail.com>
2021-09-29 17:49:03 -0300

    Movimento da bola

---
+++ Assets/BallMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{
+    private Rigidbody2D _ballRigidbody;
+    [SerializeField] Vector2 jumpForce;
+    void Start()
+    {
+        _ballRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void Jump()
+    {
+        _ballRigidbody.velocity = Vector2.zero;
+        _ballRigidbody.AddForce(jumpForce, ForceMode2D.Impulse);
+    }
+}
